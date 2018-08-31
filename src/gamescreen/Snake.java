36a6678cf915fc4737f10c575346d4d7195d04d2
Batman Bruce Wayne <taideptrai02@gmail.com>
2018-08-31 12:05:31 +0700

    done bait and board

--- src/gamescreen/Snake.java
+++ src/gamescreen/Snake.java
@@ -1,5 +1,22 @@
 package gamescreen;
 
-public class Snake extends Character{
+import java.awt.Color;
+import java.awt.Graphics;
+import java.util.ArrayList;
 
+public class Snake extends Character{
+	private ArrayList<Square> snake = new ArrayList<>();
+	private Color color;
+	
+	public void draw(Graphics g) {
+		g.setColor(color);
+		for (Square square : snake) {
+			square.draw(g);
+	    }
+	}
+	
+	public boolean alive() {
+		return true;
+	}
+	
 }
